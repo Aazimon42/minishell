5d11d6045b8c5a73fe21b5fd2bafd5855f62031a
Enzo Di Maiolo <edi-maio@3D5.42angouleme.fr>
2026-02-10 19:56:43 +0100

    pushez les mecs

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: edi-maio <edi-maio@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 17:14:01 by edi-maio          #+#    #+#             */
-/*   Updated: 2026/02/04 15:03:01 by malebrun         ###   ########.fr       */
+/*   Updated: 2026/02/10 17:37:24 by edi-maio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,4 +74,6 @@ void		print_error(char *str);
 void		builtinexit(t_instru *instru, t_envar *head);
 char		*get_var(t_envar *head, char *name, int i);
 void		handle_envar(t_instru *head, t_envar *envar);
+char	*ft_unquote(char *s1, int size);
+
 #endif
--- srcs/envarhandler.c
+++ srcs/envarhandler.c
@@ -6,7 +6,7 @@
 /*   By: malebrun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 08:03:35 by malebrun          #+#    #+#             */
-/*   Updated: 2026/02/10 14:56:13 by malebrun         ###   ########.fr       */
+/*   Updated: 2026/02/10 19:01:01 by edi-maio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static char	*get_evname(char *str)
 	i = 0;
 	if (str[0] == '$')
 		str++;
-	while (str[i] && str[i] != ' ')
+	while (str[i] && str[i] != ' ' && ft_isalnum(str[i]))
 		i++;
 	result = malloc(sizeof(char) * (i + 1));
 	if (!result)
@@ -47,12 +47,12 @@ static char	*get_evvalue(char *str, t_envar *envar)
 	return (NULL);
 }
 
-static void replaceenvar(t_instru *head, char *name, char *value, unsigned long pos)
+static void	replaceenvar(t_instru *head, char *name, char *value, int pos)
 {
-	int i;
-	unsigned long j;
-	int k;
-	char *result;
+	int		i;
+	int		j;
+	int		k;
+	char	*result;
 
 	k = 0;
 	j = 0;
@@ -64,7 +64,7 @@ static void replaceenvar(t_instru *head, char *name, char *value, unsigned long 
 	result = malloc(ft_strlen(head->str) - ft_strlen(name) + ft_strlen(value) + 1);
 	if (!result)
 		return ;
-	while (i < (int)pos)
+	while (i < pos)
 		result[j++] = head->str[i++];
 	k = 0;
 	while (value[k])
@@ -77,41 +77,36 @@ static void replaceenvar(t_instru *head, char *name, char *value, unsigned long 
 	free(name);
 	head->str = result;
 }
+
 static void	expand(t_instru *head, t_envar *envar)
 {
-	int	i;
-	int inbadquote;
-	unsigned long	nbdollar;
-	char *temp;
+	int		i;
+	int		inbadquote;
+	char	*temp;
 
 	inbadquote = 0;
 	i = 0;
-	nbdollar = 0;
 	while (head->str[i])
 	{
-		if (head->str[i] == '$')
-			nbdollar++;
-		if ((i == 0 && head->str[i] == '\"') || (i > 0 && head->str[i] == '\"' && head->str[i - 1] != '\\'))
+		if ((i == 0 && head->str[i] == '\'')
+			|| (i > 0 && head->str[i] == '\'' && head->str[i - 1] != '\\'))
 			inbadquote++;
-		if ((head->str[i] == '$' && head->str[i + 1] && head->str[i + 1] != ' ') && inbadquote % 2 == 0)
+		if ((head->str[i] == '$' && head->str[i + 1] && head->str[i + 1] != ' ')
+			&& inbadquote % 2 == 0)
 		{
 			temp = get_evname(&head->str[i]);
-			printf("\nprereplacement = %s", head->str);
-			replaceenvar(head, temp,get_evvalue(temp, envar), i);
-			printf("\nreplaced envar = %s", head->str);
-			nbdollar = 0;
-			inbadquote = 0;
-			i++;
-		} else {
+			replaceenvar(head, temp, get_evvalue(temp, envar), i);
+			free(head->unquoted);
+			head->unquoted = ft_unquote(head->str, ft_strlen(head->str));
 			i++;
 		}
+		else
+			i++;
 	}
 }
 
 void	handle_envar(t_instru *head, t_envar *envar)
 {
-	if (head && head->next && head->type != TEXT && head->next->type == TEXT)
-		head = head->next;
 	while (head && head->type == TEXT)
 	{
 		expand(head, envar);
